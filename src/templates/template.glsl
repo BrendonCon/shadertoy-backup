b6b511d51463966213d5c1fa242676950d7a43d9
Brendon <brendoncon@gmail.com>
2020-05-10 02:57:08 +0200

    Update template

--- src/templates/template.glsl
+++ src/templates/template.glsl
@@ -5,13 +5,11 @@ uniform float u_time;
 uniform vec2 u_mouse;
 
 void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution;
-  
-  vec2 st = gl_FragCoord.xy / u_resolution.y - 0.5;
-  st.x *= u_resolution.x / u_resolution.y;
+  vec2 uv = gl_FragCoord.xy / u_resolution - 0.5;
+  uv.x *= u_resolution.x / u_resolution.y;
   
   float t = u_time;
-  vec3 color = vec3(0.0);
+  vec4 color = vec4(0.0);
 
-  gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = color;
 }
